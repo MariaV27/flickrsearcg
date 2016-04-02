6a022407e2cf6a035bce1830174f936c3e21b427
Maria Vintimilla <mariavintimilla@Marias-MacBook-Air.local>
2016-04-02 12:37:38 -0400

    flikr api

--- public/app.js
+++ public/app.js
@@ -1,21 +1,23 @@
 angular.module('githubApp', ['ngTable'])
 .controller('GithubController', function($scope, $http, $filter, NgTableParams) {
-  $scope.githubTable = new NgTableParams({
+  $scope.flickrTable = new NgTableParams({
     orderby: {
       full_name: 'asc'
     }
   }, {
     getData: function($defer, params) {
-      return $http.get('https://api.github.com/users/' + $scope.username + '/repos')
+      // return $http.get('https://api.github.com/users/' + $scope.username + '/repos')
+      return $http.get('https://api.flickr.com/services/rest/?api_key=5d0b99b598780adb1ce7f682110a03e6&method=flickr.photos.search&format=json&nojsoncallback=1&text=' + $scope.search)
       .then(function(response) {
-        var filteredData = $filter('filter')(response.data, params.filter())
+        var photos = response.data.photos.photo;
+        var filteredData = $filter('filter')(photos, params.filter())
         var sortedData = $filter('orderBy')(filteredData, params.orderBy());
         return sortedData;
       });
     }
   });
 
-  $scope.loadRepos = function() {
-    $scope.githubTable.reload();
+  $scope.loadPhotos = function() {
+    $scope.flickrTable.reload();
   }
 });
